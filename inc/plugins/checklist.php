505a0a14387c65ea36326b8821e82855863a0f6a
Julia <midnightwhisper@web.de>
2017-05-28 04:28:42 +0200

    Update checklist.php

--- inc/plugins/checklist.php
+++ inc/plugins/checklist.php
@@ -4,7 +4,7 @@ if(!defined("IN_MYBB"))
 	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
 }
 
-$plugins->add_hook("admin_formcontainer_output_row", "checklist_checkbox");
+$plugins->add_hook("global_intermediate", "checklist_global");
 
 function checklist_info()
 {
@@ -22,9 +22,9 @@ function checklist_info()
 function checklist_install()
 {
   global $db, $cache, $mybb;
-  
-  $db->query("ALTER TABLE `".TABLE_PREFIX."profilefields` ADD `checklist` int(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `allowvideocode`;");
-  
+
+  $db->query("ALTER TABLE `".TABLE_PREFIX."profilefields` ADD `checklist` int(1)  NOT NULL DEFAULT '1';");
+
   $setting_group = array(
     'name' => 'checklist',
     'title' => 'Bewerber-Checklist',
@@ -33,38 +33,52 @@ function checklist_install()
     'isdefault' => 0
   );
   $gid = $db->insert_query("settinggroups", $setting_group);
-  
+
   $setting_array = array(
+    'checklist_group' => array(
+    'title' => 'Benutzergruppe für Bewerber',
+    'description' => 'Wie lautet die Gruppen-ID der Bewerber?',
+    'optionscode' => 'text',
+    'value' => '996', // Default
+    'disporder' => 1
+     ),
+    'checklist_fields' => array(
+    'title' => 'Benötigte Profilfelder',
+    'description' => 'Gib hier die IDs der benötigten Profilfelder ein. Trennen mit <strong>", "</strong>!',
+    'optionscode' => 'text',
+    'value' => '997, 998', // Default
+    'disporder' => 2
+     ),
    'checklist_application' => array(
    'title' => 'Steckbrief voraussetzen?',
    'description' => 'Brauchen Bewerber in deinem Forum einen Steckbrief?',
    'optionscode' => 'yesno',
    'value' => 1,
-   'disporder' => 1
+   'disporder' => 3
     ),
     'checklist_forum' => array(
     'title' => 'Unterforum für Bewerbungen',
     'description' => 'Gib die ID deines Unterforums für Bewerbungen an.',
     'optionscode' => 'text',
     'value' => '999', // Default
-    'disporder' => 2
+    'disporder' => 4
      ),
     'checklist_birthday' => array(
-        'title' => 'Geburtstag voraussetzen?',
-        'description' => 'Müssen Bewerber in deinem Forum ihr Geburtstag im Profil angeben (mit MyBB-Standardfunktion)?',
-        'optionscode' => 'yesno',
-        'value' => 1,
-        'disporder' => 3
+    'title' => 'Geburtstag voraussetzen?',
+    'description' => 'Müssen Bewerber in deinem Forum ihr Geburtstag im Profil angeben (mit MyBB-Standardfunktion)?',
+    'optionscode' => 'yesno',
+    'value' => 1,
+    'disporder' => 5
     ),
   );
-  
+
   foreach($setting_array as $name => $setting)
   {
     $setting['name'] = $name;
     $setting['gid'] = $gid;
      $db->insert_query('settings', $setting);
   }
-  
+
   rebuild_settings();
 }
 
@@ -81,38 +95,66 @@ function checklist_is_installed()
 function checklist_uninstall()
 {
   global $db;
-  
+
   if($db->field_exists("checklist", "profilefields"))
   {
     $db->drop_column("profilefields", "checklist");
   }
-  
-  $db->delete_query('settings', "name IN('checklist_application', 'checklist_forum', 'checklist_birthday')");
+
+  $db->delete_query('settings', "name IN('checklist_group', 'checklist_fields', 'checklist_application', 'checklist_forum', 'checklist_birthday')");
   $db->delete_query('settinggroups', "name = 'checklist'");
-  
+
   rebuild_settings();
 }
 
 function checklist_activate()
 {
   global $db, $mybb;
-  
+
 }
 
 function checklist_deactivate()
 {
   global $db, $mybb;
-  
+
 }
 
-function checklist_checkbox($above)
+function checklist_global() 
 {
-	global $mybb, $lang, $form_container, $forum_data, $form;
-	if($above['title'] == $lang->edit_profile_field || $lang->add_profile_field)
-	{
-		$above['content'] .= $form_container->output_row("Pflicht für Annahme des Charakters?", "Muss dieses Profilfeld ausgefüllt sein, damit der Charakter angenommen wird?", $form->generate_yes_no_radio('checklist', $mybb->input['checklist']));
-	}
-	return $above;
+  global $db, $mybb, $lang, $templates, $header_checklist;
+  $uid = $mybb->user['uid'];
+  if($mybb->usergroup['uid'] == $mybb->settings['checklist_group']) {
+    $fields = explode(", ", $db->escape_string($mybb->settings['checklist_fields']));
+    foreach($fields as $db->escape_string($field)) {
+      $query = $db->query("SELECT fid, name, length FROM ".TABLE_PREFIX."profilefields WHERE fid = '$field'");
+      $field = $db->fetch_array($query);
+      $fid = "fid".$field['fid'];
+      if(empty($mybb->user[$fid]) || strln($mybb->user[$fid] < $field['length']) {
+        eval("\$checklist_check .= \"".$templates->get("checklist_field_checked")."\";");
+      }
+      else {
+        eval("\$checklist_check .= \"".$templates->get("checklist_field_unchecked")."\";");
+      }
+    }
+    if($mybb->settings['checklist_birthday'] == "1") {
+      if(!empty($mybb->user['birthday'])) {
+        eval("\$checklist_check .= \"".$templates->get("checklist_birthday_checked")."\";");
+      }
+      else {
+        eval("\$checklist_check .= \"".$templates->get("checklist_birthday_unchecked")."\";");
+      }
+    }
+    if($mybb->settings['checklist_application'] == "1") {
+      $query = $db->simple_select("threads", "*", "uid = '".$uid."'");
+      $application = $db->fetch_array($query);
+      if(!empty($application)) {
+        eval("\$checklist_check .= \"".$templates->get("checklist_application_checked")."\";");
+      }
+      else {
+        eval("\$checklist_check .= \"".$templates->get("checklist_application_unchecked")."\";");
+      }
+    }
+    eval("\$header_checklist .= \"".$templates->get("checklist")."\";");
+  }
 }
-
 ?>
