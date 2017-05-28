4a27c6bbc4fb3256d095873eb4f493c18879007a
Julia <midnightwhisper@web.de>
2017-05-28 05:49:05 +0200

    Update checklist.lang.php

--- inc/deutsch_du/checklist.lang.php
+++ inc/deutsch_du/checklist.lang.php
@@ -1,11 +1,13 @@
 <?php
 $l['checklist'] = "Bewerber-Checklist";
+$l['checklist_avatar_checked'] = "Du hast deinem Charakter <strong>ein Gesicht</strong> verliehen!";
+$l['checklist_avatar_unchecked'] = "Vergiss nicht, <strong>einen Avatar</strong> hochzuladen!";
 $l['checklist_field_checked'] = "Das Feld wurde ausgefüllt!";
 $l['checklist_field_unchecked'] = "Das Feld wurde nicht oder nicht korrekt (keine Beachtung der Mindestlänge) ausgefüllt!";
-$l['checklist_birthday_checked'] = "Du hast den Geburtstag deines Charakters angegeben!";
-$l['checklist_birthday_unchecked'] = "Bitte trage den Geburtstag deines Charakters ein!";
-$l['checklist_application_checked'] = "Du hast deinen Steckbrief gepostet!";
-$l['checklist_application_unchecked'] = "Vergiss nicht, deinen Steckbrief zu posten!";
+$l['checklist_birthday_checked'] = "Du hast den <strong>Geburtstag</strong> deines Charakters angegeben!";
+$l['checklist_birthday_unchecked'] = "Bitte trage den <strong>Geburtstag</strong> deines Charakters ein!";
+$l['checklist_application_checked'] = "Du hast <strong>deinen Steckbrief</strong> gepostet!";
+$l['checklist_application_unchecked'] = "Vergiss nicht, <strong>deinen Steckbrief</strong> zu posten!";
 $l['checklist_forum'] = "Zum Unterforum";
 $l['checklist_application'] = "Deine Bewerbung";
 $l['checklist_sep'] = "<strong>&rarr;</strong>";
